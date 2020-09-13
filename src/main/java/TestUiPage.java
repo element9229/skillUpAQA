1b003d772fa55fd73bbde96859d64b70ac751f46
element9229 <tarassokal1@gmail.com>
2020-09-13 16:39:18 +0300

    myFirstCommit

---
+++ src/main/java/TestUiPage.java
@@ -0,0 +1,72 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import utils.ConfigProperties;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestUiPage {
+
+    public static void main (String[] args) throws IOException, InterruptedException {
+
+//        System.out.println("enter number");
+//        Scanner readFromClientTerminal = new Scanner(System.in);
+//        int webSite = readFromClientTerminal.nextInt();
+
+        String webUrl = ConfigProperties.getProperty("webUrl");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get(webUrl);
+
+        WebElement element = driver.findElement(By.cssSelector("[role='combobox']"));
+        element.click();
+        element.clear();
+        element.sendKeys("Selenium");
+        element.sendKeys(Keys.ENTER);
+
+        List<WebElement> searchResults = driver.findElements(By.xpath("//div[@class='rc']"));
+
+        if (searchResults.size() == 9) {
+            System.out.println("The result is correct");
+        } else {
+            System.out.println("The result is not correct");
+        }
+
+//        for (int i =0; i<= searchResults.size()-1; i++) {
+//            System.out.println(searchResults.get(i).getText());
+//            System.out.println("\n");
+//        }
+
+        List<Boolean> toCheck = new ArrayList<Boolean>();
+
+        for (WebElement result : searchResults) {
+            String textFromSearchResult = result.getText();
+            System.out.println(textFromSearchResult);
+            System.out.println("\n");
+
+            if (textFromSearchResult.contains("Selenium")) {
+                toCheck.add(true);
+            } else {
+                toCheck.add(false);
+            }
+        }
+
+        for (Boolean result : toCheck) {
+            System.out.println(result);
+        }
+
+        Thread.sleep( 5000);
+
+        driver.quit();
+
+    }
+}
---
+++ src/main/java/utils/ConfigProperties.java
@@ -0,0 +1,17 @@
+package utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigProperties {
+
+    public static String getProperty(String property) throws IOException {
+        InputStream input =
+                ConfigProperties.class.getClassLoader().getResourceAsStream("config.properties");
+
+        Properties properties = new Properties();
+        properties.load(input);
+        return properties.getProperty(property);
+    }
+}
