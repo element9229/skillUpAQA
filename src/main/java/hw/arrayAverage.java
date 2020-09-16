37f9d16fc118c69916eb7339f8bcfbd9117f7ad8
Taras <tarassokal1@gmail.com>
2020-09-17 00:10:22 +0300

    My pull request (#3)
    * hw
    
    * Homework finalization

--- src/main/java/hw/arrayAverage.java
+++ src/main/java/hw/arrayAverage.java
@@ -1,6 +1,10 @@
 package hw;
 
 public class arrayAverage {
+<<<<<<< HEAD
+
+=======
+>>>>>>> 926673e9c0e94f60d9d2767e4b2f7f8468b44f1a
     public static void main(String[] args) {
         int[] numbers = {-5,-15,21,13,27,32,44,51};
 
@@ -11,7 +15,20 @@ public class arrayAverage {
                 sum += numbers[i];
             }
             average = sum / numbers.length;
+<<<<<<< HEAD
+            System.out.println("Average : " + average);
+
+            for (int i = 0; i < numbers.length; i++) {
+                if (numbers[i] < average) {
+                    System.out.println("Numbers less than average : " + numbers[i]);
+                }
+            }
+        }
+    }
+}
+=======
             System.out.println("result - " + average);
             }
         }
     }
+>>>>>>> 926673e9c0e94f60d9d2767e4b2f7f8468b44f1a
